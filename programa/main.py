8d7c57f50e59004429254fdbae7b89821f6ca506
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2021-12-03 16:47:00 +0000

    Update main.py

--- programa/main.py
+++ programa/main.py
@@ -1,38 +1,86 @@
-menu_options = {
+menuPrincipalOptions = {
     1: 'Imagens',
     2: 'Áudio',
-    3: 'Compressão',
+    3: 'Vídeo',
+    4: 'Compressão',
+    5: 'Sair'
+}
+
+menuImagensOptions = {
+    1: 'Operações Aritméticas',
+    2: 'Operações Lógicas',
+    3: 'Filtros',
     4: 'Sair',
 }
 
-def print_menu():
-    for key in menu_options.keys():
-        print (key, '--', menu_options[key] )
+menuAudioOptions = {
+    1: 'Edição',
+    2: 'Filtros',
+    3: 'Sair',
+}
 
-def option1():
-     print('opção 1')
+menuCompressaoOptions = {
+    1: 'Compressão de imagem',
+    2: 'Compressão de vídeo',
+    3: 'Sair',
+}
 
-def option2():
-     print('opção 2')
+def print_menuPrincipal():
+    for key in menuPrincipalOptions.keys():
+        print (key, '--', menuPrincipalOptions[key] )
 
-def option3():
-     print('opção 3')
+def print_menuImagens():
+    for key in menuImagensOptions.keys():
+        print (key, '--', menuAudioOptions[key] )
+
+def print_menuAudio():
+    for key in menuImagensOptions.keys():
+        print (key, '--', menuImagensOptions[key] )
+
+def print_menuCompressao():
+    for key in menuCompressaoOptions.keys():
+        print (key, '--', menuCompressaoOptions[key] )
+
+def videoPretoBranco():
+    print('Vídeo a preeto e branco')
+
+def imagemAdicao():
+    print('Adição de imagens')
+
+def imagemSubtracao():
+    print('Subtração de imagens')
 
 if __name__=='__main__':
     while(True):
-        print_menu()
+        print_menuPrincipal()
         option = ''
         try:
             option = int(input('Escolha uma opção: '))
         except:
             print('Input inválido, por favor escolha um número...')
         if option == 1:
-           option1()
+            print_menuImagens()
+            option = ''
+            try:
+                option = int(input('Escolha uma opção: '))
+            except:
+                print('Input inválido, por favor escolha um número...')
+            if option == 1:
+                
+            elif option == 2:
+                
+            elif option == 3:
+
+            else
+                exit()
+
         elif option == 2:
-            option2()
+            print_menuAudio()
         elif option == 3:
-            option3()
+            videoPretoBranco()
         elif option == 4:
+            print_menuCompressao()
+        elif option == 5:
             exit()
         else:
-            print('Opção inválida, escolha um número entre 1 e 4.')
+            print('Opção inválida, escolha um número entre 1 e 5.')

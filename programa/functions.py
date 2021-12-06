dd502e10e96a26f53bb33b898f5144a4b2ebf583
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2021-12-06 12:05:45 +0000

    Corrigi preto e branco e adicionei comments nas minhas partes

--- programa/functions.py
+++ programa/functions.py
@@ -4,50 +4,66 @@ from PIL import ImageEnhance
 from time import sleep
 
 def imagemMultiplicacao(pathImagem1, pathImagem2):
+    # Abrir imagens
     imagem1 = Image.open(pathImagem1)
     imagem2 = Image.open(pathImagem2)
-
+    
+    # Criar nova imagem onde será guardado o resultado
     imagemMult = Image.new(imagem1.mode, imagem1.size, 'white')
     imagemMult.save("ImagemMultiplicacao.jpg")
 
-    for i in range(0, imagem1.size[0]-1):
-        for j in range(0, imagem1.size[1]-1):
-            pixelColorsValsImagem1 = imagem1.getpixel((i,j))
-            pixelColorsValsImagem2 = imagem2.getpixel((i,j))
+    # Precorrer imagem pixel a pixel
+    for x in range(0, imagem1.size[0]-1):
+        for y in range(0, imagem1.size[1]-1):
+            # Para cada imagem, armazenar os dados do pixel
+            pixelColorsValsImagem1 = imagem1.getpixel((x,y))
+            pixelColorsValsImagem2 = imagem2.getpixel((x,y))
 
+            # Criar novos valores para o RGB -> Multiplicando o valor de cada R,G,B de cada uma das imagens
             redPixel = pixelColorsValsImagem1[0] * pixelColorsValsImagem2[0]
             greenPixel = pixelColorsValsImagem1[1] * pixelColorsValsImagem2[1]
             bluePixel = pixelColorsValsImagem1[2] * pixelColorsValsImagem2[2]
 
-            imagemMult.putpixel((i,j), (redPixel, greenPixel, bluePixel))
+            # Colocar em cada pixel da imagem criada, o pixel com os novos valores de RGB
+            imagemMult.putpixel((x,y), (redPixel, greenPixel, bluePixel))
     
+    # Guardar nova imagem e mostrar aviso ao utilizador
     imagemMult.save('ImagemMultiplicacao.jpg')
+
     print('Multiplicação de imagens realizada com sucesso --> Verificar ImagemMultiplicação.jpg')
     sleep(4)
 
 def imagemSubtracao(pathImagem1, pathImagem2):
+    # Abrir imagens
     imagem1 = Image.open(pathImagem1)
     imagem2 = Image.open(pathImagem2)
 
+    # Criar nova imagem onde será guardado o resultado
     imagemSoma = Image.new(imagem1.mode, imagem1.size, 'white')
     imagemSoma.save("ImagemSubt.jpg")
 
-    for i in range(0, imagem1.size[0]-1):
-        for j in range(0, imagem1.size[1]-1):
-            pixelColorsValsImagem1 = imagem1.getpixel((i,j))
-            pixelColorsValsImagem2 = imagem2.getpixel((i,j))
+    # Precorrer imagem pixel a pixel
+    for x in range(0, imagem1.size[0]-1):
+        for y in range(0, imagem1.size[1]-1):
+            # Para cada imagem, armazenar os dados do pixel
+            pixelColorsValsImagem1 = imagem1.getpixel((x,y))
+            pixelColorsValsImagem2 = imagem2.getpixel((x,y))
 
+            # Criar novos valores para o RGB -> Subtraindo o valor de cada R,G,B de cada uma das imagens
             redPixel = pixelColorsValsImagem1[0] - pixelColorsValsImagem2[0]
             greenPixel = pixelColorsValsImagem1[1] - pixelColorsValsImagem2[1]
             bluePixel = pixelColorsValsImagem1[2] - pixelColorsValsImagem2[2]
 
-            imagemSoma.putpixel((i,j), (redPixel, greenPixel, bluePixel))
+            # Colocar em cada pixel da imagem criada, o pixel com os novos valores de RGB
+            imagemSoma.putpixel((x,y), (redPixel, greenPixel, bluePixel))
     
+    # Guardar nova imagem e mostrar aviso ao utilizador
     imagemSoma.save('ImagemSubt.jpg')
     print('Subtração de imagens realizada com sucesso --> Verificar ImagemSubt.jpg')
     sleep(4)
 
 def imagemNegativo(pathImagem1):
+    # Abrir imagem
     imagem1 = Image.open(pathImagem1)
 
     # Criar a Imagem Resultado
@@ -55,12 +71,11 @@ def imagemNegativo(pathImagem1):
     imagem_negativo.save("ImagemNegativo.jpg")
     
     # Negativo - Algoritmo
-
-    for i in range(0, imagem1.size[0] - 1):
-        for j in range(0, imagem1.size[1] - 1):
+    for x in range(0, imagem1.size[0] - 1):
+        for y in range(0, imagem1.size[1] - 1):
 
             # Encontrar o valor do pixel na posição (x,y) da imagem
-            pixelColorVals = imagem1.getpixel((i,j))
+            pixelColorVals = imagem1.getpixel((x,y))
 
             # Inverter a Cor
             redPixel = 255 - pixelColorVals[0]  # Negativo do pixel vermelho
@@ -68,13 +83,13 @@ def imagemNegativo(pathImagem1):
             bluePixel = 255 - pixelColorVals[2] # Negativo do pixel azul
 
             # Modificar a imagem com os pixeis invertidos
-            imagem_negativo.putpixel((i,j) (redPixel, greenPixel, bluePixel))
+            imagem_negativo.putpixel((x,y), (redPixel, greenPixel, bluePixel))
 
+    # Guardar nova imagem e mostrar aviso ao utilizador
     imagem_negativo.save("ImagemNegativo.jpg")
-
-    
-
-    
+    print('Negativo de uma imagem realizado com sucesso --> Verificar ImagemNegativo.jpg')
+    sleep(4)
+  
 def imagemAND():
     print('AND de imagens')
 
@@ -82,14 +97,24 @@ def imagemThreshold():
     print('Threshold de uma imagem')
 
 def imagemPretoBranco(pathimagem1):
+    # Abrir imagem
     image = Image.open(pathimagem1)
+
+    # recolher dados da imagem
     img_data = image.getdata()
 
+    # Criar lista em que são colocados os pixeis da imagem lida, transformados para valores de preto e branco
     lst=[]
     for pixel in img_data:
         lst.append(pixel[0]*0.2125+pixel[1]*0.7174+pixel[2]*0.0721)
+
+    # Criar nova imagem do mesmo tamanho da imagem lida
     new_img = Image.new("L", image.size)
+
+    # Colocar na nova imagem o conteúdo da lista
     new_img.putdata(lst)
+
+    # Guardar nova imagem e mostrar aviso ao utilizador
     new_img.save("ImagemPretoBranco.jpg")
     print('Preto e branco da imagem realizado com sucesso --> Verificar ImagemPretoBranco.jpg')
     sleep(4)

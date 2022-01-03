cfdbec813643f95ce906741e0b1f2fefe763f083
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-01-03 00:28:43 +0000

    Update functions.py

--- programa/functions.py
+++ programa/functions.py
@@ -4,6 +4,8 @@ from PIL import ImageEnhance
 from time import sleep
 from pydub import AudioSegment
 import math
+import os
+from pydub import ffmpeg
 
 
 def imagemMultiplicacao(pathImagem1, pathImagem2):
@@ -260,8 +262,38 @@ def videoPretoBranco():
 def comprimeImagem():
     print('Comprime uma imagem')
 
-def comprimeVideo():
-    print('Comprime um video')
+def comprimeVideo(video_full_path, output_file_name, target_size):
+    # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+
+    probe = ffmpeg.probe(video_full_path)
+    # Video duration, in s.
+    duration = float(probe['format']['duration'])
+    # Audio bitrate, in bps.
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+    # Target total bitrate, in bps.
+    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
+
+    # Target audio bitrate, in bps
+    if 10 * audio_bitrate > target_total_bitrate:
+        audio_bitrate = target_total_bitrate / 10
+        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+            audio_bitrate = min_audio_bitrate
+        elif audio_bitrate > max_audio_bitrate:
+            audio_bitrate = max_audio_bitrate
+    # Target video bitrate, in bps.
+    video_bitrate = target_total_bitrate - audio_bitrate
+
+    i = ffmpeg.input(video_full_path)
+    ffmpeg.output(i, os.devnull,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                  ).overwrite_output().run()
+    ffmpeg.output(i, output_file_name,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                  ).overwrite_output().run()
+    print("Video comprimido com sucesso, verificar videoComprimido.mp4")
+    sleep(3)
 
 menuPrincipalOptions = {
     1: 'Imagens',

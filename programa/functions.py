e08d802034028d94e42972ef88878ed3c0359556
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-01-03 20:52:14 +0000

    reduzi o número de cores da imagem comprimida para que esta consiga ter um tamanho inferior à primeira

--- programa/functions.py
+++ programa/functions.py
@@ -12,6 +12,50 @@ except ImportError:
     sys.exit()
 
 
+# Defenições de compressão de Imagem
+def initialize_K_centroids(X, K):
+    """ Choose K points from X at random """
+    m = len(X)
+    return X[numpy.random.choice(m, K, replace=False), :]
+
+def find_closest_centroids(X, centroids):
+    m = len(X)
+    c = numpy.zeros(m)
+    for i in range(m):
+        # Find distances
+        distances = numpy.linalg.norm(X[i] - centroids, axis=1)
+        # Assign closest cluster to c[i]
+        c[i] = numpy.argmin(distances)
+    return c
+
+def compute_means(X, idx, K):
+    _, n = X.shape
+    centroids = numpy.zeros((K, n))
+    for k in range(K):
+        examples = X[numpy.where(idx == k)]
+        mean = [numpy.mean(column) for column in examples.T]
+        centroids[k] = mean
+    return centroids
+
+def find_k_means(X, K, max_iters=10):
+    centroids = initialize_K_centroids(X, K)
+    previous_centroids = centroids
+    for _ in range(max_iters):
+        idx = find_closest_centroids(X, centroids)
+        centroids = compute_means(X, idx, K)
+        if (centroids == previous_centroids).all():
+            # The centroids aren't moving anymore.
+            return centroids
+        else:
+            previous_centroids = centroids
+
+    return centroids, idx
+
+def load_image(path):
+    """ Load image from path. Return a numpy array """
+    image = Image.open(path)
+    return numpy.asarray(image) / 255
+
 def imagemMultiplicacao(pathImagem1, pathImagem2):
     # Abrir imagens
     imagem1 = Image.open(pathImagem1)
--- programa/main.py
+++ programa/main.py
@@ -1,49 +1,6 @@
 from functions import *
 import numpy 
-# Defenições de compressão de Imagem
-def initialize_K_centroids(X, K):
-    """ Choose K points from X at random """
-    m = len(X)
-    return X[numpy.random.choice(m, K, replace=False), :]
-
-def find_closest_centroids(X, centroids):
-    m = len(X)
-    c = numpy.zeros(m)
-    for i in range(m):
-        # Find distances
-        distances = numpy.linalg.norm(X[i] - centroids, axis=1)
-        # Assign closest cluster to c[i]
-        c[i] = numpy.argmin(distances)
-    return c
-
-def compute_means(X, idx, K):
-    _, n = X.shape
-    centroids = numpy.zeros((K, n))
-    for k in range(K):
-        examples = X[numpy.where(idx == k)]
-        mean = [numpy.mean(column) for column in examples.T]
-        centroids[k] = mean
-    return centroids
-
-def find_k_means(X, K, max_iters=10):
-    centroids = initialize_K_centroids(X, K)
-    previous_centroids = centroids
-    for _ in range(max_iters):
-        idx = find_closest_centroids(X, centroids)
-        centroids = compute_means(X, idx, K)
-        if (centroids == previous_centroids).all():
-            # The centroids aren't moving anymore.
-            return centroids
-        else:
-            previous_centroids = centroids
-
-    return centroids, idx
-
-def load_image(path):
-    """ Load image from path. Return a numpy array """
-    image = Image.open(path)
-    return numpy.asarray(image) / 255
-
+import os
 
 if __name__=='__main__':
     clearConsole()
@@ -196,15 +153,20 @@ if __name__=='__main__':
                 image_path = ('images/cao.jpg')
                 image = load_image(image_path)
                 w, h, d = image.shape
-                print('Image found with width: {}, height: {}, depth: {}'.format(w, h, d))
                 X = image.reshape((w * h, d))
-                K = 20 # the desired number of colors in the compressed image
+                K = 4 # the desired number of colors in the compressed image
                 colors, _ = find_k_means(X, K, max_iters=20)
                 idx = find_closest_centroids(X, colors)
                 idx = numpy.array(idx, dtype=numpy.uint8)
                 X_reconstructed = numpy.array(colors[idx, :] * 255, dtype=numpy.uint8).reshape((w, h, d))
                 compressed_image = Image.fromarray(X_reconstructed)
                 compressed_image.save('images/imagemCompressao.png')
+                tamanhoImagem = os.stat('images/cao.jpg').st_size
+                tamanhoImagemComp = os.stat('images/imagemCompressao.png').st_size
+                razao = tamanhoImagem/tamanhoImagemComp
+                print("Tamanho do imagem original: {0}KB\nTamanho da imagem comprimida: {1}KB\nTaxa de compressão: {2}".format(tamanhoImagem, tamanhoImagemComp, razao))
+                sleep(5)
+
 
             elif option == 2:
                 comprimeVideo()

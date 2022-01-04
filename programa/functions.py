3ae2977c99e11ad13d7033ed80cffbbce41d32fb
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-01-04 12:20:07 +0000

    videoPretoeBranco com cv2

--- programa/functions.py
+++ programa/functions.py
@@ -310,7 +310,28 @@ def audioAcelerar(pathAudio, speed):
     print("Som acelerado com sucesso, verificar sound2Acelerado.wav")
     sleep(3)
 
-
+def videoPretoBranco(videopath):
+    source = cv2.VideoCapture(videopath)
+    while True:
+        # extracting the frames
+        ret, frame = source.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+        # displaying the video
+        cv2.imshow("Live", gray)
+    
+        # exiting the loop
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            break
+    # closing the window
+    cv2.destroyAllWindows()
+    source.release()
 
 def comprimeVideo(video_full_path, output_file_name, target_size):
     ## O target é o tamanho do video comprimido desejado, em MB
